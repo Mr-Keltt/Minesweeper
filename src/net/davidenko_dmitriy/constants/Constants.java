be266571e0231e828f4328e889c1bcd471920333
MrKeltt <dmitriydavidenkokeltt@gmail.com>
2022-11-15 20:31:24 +0300

    A class of Constants has been created

---
+++ src/net/davidenko_dmitriy/constants/Constants.java
@@ -0,0 +1,20 @@
+package net.davidenko_dmitriy.constants;
+
+public class Constants {
+    public static final int Window_Cord_X = 150;
+    public static final int Window_Cord_Y = 150;
+    public static final int Settings_Window_Width = 400;
+    public static final int Settings_Window_Height = 300;
+    public static final int Min_Width_Height = 500;
+    public static final int Header_Height = 200;
+    public static final int Cell_Edge_Length = 40;
+    public static final int Beginner_Width = 10;
+    public static final int Beginner_Height = 10;
+    public static final int Beginner_Bombs_Count = 10;
+    public static final int Amateur_Width = 16;
+    public static final int Amateur_Height = 16;
+    public static final int Amateur_Bombs_Count = 40;
+    public static final int Professional_Width = 30;
+    public static final int Professional_Height = 16;
+    public static final int Professional_Bombs_Count = 99;
+}
