676ca7f118ac3acd73f083fde7cebe25b484bcb1
MrKeltt <dmitriydavidenkokeltt@gmail.com>
2022-11-16 21:00:31 +0300

    -added new constants needed for the interface -added the functions needed to create and add a header

--- src/net/davidenko_dmitriy/constants/Constants.java
+++ src/net/davidenko_dmitriy/constants/Constants.java
@@ -1,12 +1,15 @@
 package net.davidenko_dmitriy.constants;
 
-public class Constants {
+import java.awt.*;
+import java.io.InputStream;
+
+public abstract class Constants {
+    public static final int Min_Game_Window_Width = 500;
     public static final int Window_Location_X = 150;
     public static final int Window_Location_Y = 150;
-    public static final int Settings_Window_Width = 400;
-    public static final int Settings_Window_Height = 300;
-    public static final int Min_Game_Window_Width = 500;
-    public static final int Header_Height = 200;
+    public static final int Header_Height = 70;
+    public static final int Score_Width = 130;
+    public static final int Score_Height = 50;
     public static final int Cell_Edge_Length = 40;
     public static final int Beginner_Horizontal_Size = 10;
     public static final int Beginner_Vertical_Size = 10;
@@ -17,4 +20,13 @@ public class Constants {
     public static final int Professional_Horizontal_Size = 30;
     public static final int Professional_Vertical_Size = 16;
     public static final int Professional_Bombs_Count = 99;
+    public static final int Settings_Window_Width = 400;
+    public static final int Settings_Window_Height = 300;
+
+    public static final Color Header_Background = new  Color(200, 200, 200);
+    public static final Color Score_Background = new  Color(70,0,0);
+    public static final Color Score_Text_Color = new  Color(255,0,0);
+
+
+    public static final Font Score_Text_Font = new Font("Arial", Font.BOLD, 30);
 }
--- src/net/davidenko_dmitriy/gui/GameGUI.java
+++ src/net/davidenko_dmitriy/gui/GameGUI.java
@@ -3,25 +3,57 @@ package net.davidenko_dmitriy.gui;
 import net.davidenko_dmitriy.constants.Constants;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class GameGUI extends JFrame {
+    // Window frame sizes
+    private int frameWidth;
+    private int frameHeight;
+
+    private JLabel bombCounterValue;
+
+    private JLabel timerValue;
+
+
     // Constructor
     public GameGUI() {
+        // Setting the window name
         super("Minesweeper");
 
+
+        // Setting the starting value of variables
+        bombCounterValue = new JLabel(String.valueOf(Constants.Beginner_Bombs_Count));
+        timerValue = new JLabel("00:00:00");
+
+
+        // Calculating the size of the window frames
+        this.setSize(200,200);
+        JPanel test = new JPanel();
+        this.add(test);
+        setVisible(true);
+        frameWidth = this.getWidth() - test.getWidth();
+        frameHeight = this.getHeight() - test.getHeight();
+        this.remove(test);
+
+
+        // Setting window parameters
         setSizeByCells(Constants.Beginner_Horizontal_Size, Constants.Beginner_Vertical_Size);
         this.setLocation(Constants.Window_Location_X, Constants.Window_Location_Y);
-
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLayout(null);
 
-        this.setVisible(true);
+
+        drawingHeader();
     }
+
 
     // Set window size
     public void setSizeByCells(int horizontalSize, int verticalSize) {
         // create local variables
         int width, height;
-
 
         // calculate window width
         if (Constants.Cell_Edge_Length * horizontalSize <= Constants.Min_Game_Window_Width) {
@@ -36,6 +68,77 @@ public class GameGUI extends JFrame {
 
 
         // set window size
-        this.setSize(width, height);
+        this.setSize(width + frameWidth, height + frameHeight);
+    }
+
+
+    public void drawingHeader() {
+        // creature header
+        JPanel header = new JPanel();
+
+        // creating a bomb counter
+        JPanel bombCounter = new JPanel();
+
+        // creating a timer
+        JPanel timer = new JPanel(new BorderLayout());
+
+
+        initHeader(header);
+
+        initBombCounter(bombCounter);
+
+        initTimer(timer);
+
+
+        // adding a header to the screen
+        this.add(header);
+
+        // adding a bomb counter to a header
+        header.add(bombCounter);
+
+        // adding a timer to a header
+        header.add(timer);
+    }
+
+
+    private void initHeader(JPanel header) {
+        // setting a parameter header
+        header.setSize(new Dimension(this.getSize().width, Constants.Header_Height));
+        header.setBackground(Constants.Header_Background);
+        header.setLayout(null);
+    }
+
+    private void initBombCounter(JPanel bombCounter) {
+        // setting a parameter bomb counter
+        bombCounter.setLayout(new BorderLayout());
+        bombCounter.setSize(new Dimension(Constants.Score_Width, Constants.Score_Height));
+        bombCounter.setLocation((Constants.Header_Height - Constants.Score_Height) / 2, (Constants.Header_Height - Constants.Score_Height)/2);
+        bombCounter.setBackground(Constants.Score_Background);
+
+        // setting text display parameters
+        this.bombCounterValue.setForeground(Constants.Score_Text_Color);
+        this.bombCounterValue.setFont(Constants.Score_Text_Font);
+        this.bombCounterValue.setVerticalAlignment(JLabel.CENTER);
+        this.bombCounterValue.setHorizontalAlignment(JLabel.CENTER);
+
+        // adding text to the bomb counter
+        bombCounter.add(bombCounterValue, BorderLayout.CENTER);
+    }
+
+    private void initTimer(JPanel timer) {
+        // setting a parameter timer
+        timer.setSize(new Dimension(Constants.Score_Width, Constants.Score_Height));
+        timer.setLocation(this.getWidth() - frameWidth - (Constants.Header_Height - Constants.Score_Height) / 2 - Constants.Score_Width,
+                (Constants.Header_Height - Constants.Score_Height)/2);
+        timer.setBackground(Constants.Score_Background);
+
+        // setting text display parameters
+        this.timerValue.setForeground(Constants.Score_Text_Color);
+        this.timerValue.setFont(Constants.Score_Text_Font);
+        this.timerValue.setVerticalAlignment(JLabel.CENTER);
+        this.timerValue.setHorizontalAlignment(JLabel.CENTER);
+
+        // adding text to the timer
+        timer.add(timerValue, BorderLayout.CENTER);
     }
 }
